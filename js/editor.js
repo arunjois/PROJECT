4989734361c01201263f600b6dbc307c0c314f6b
Arun Jois <arun.joiss@gmail.com>
2018-07-03 23:28:17 +0530

    [Checkpoint] Angular Working

---
+++ js/editor.js
@@ -0,0 +1,9 @@
+var app = angular.module('editor', ['editor']);
+app.controller('code', function($scope) {
+   $scope.latex="Something";
+
+    /*  $http.post("/api/book/", data).success(function(data, status) {
+        console.log('Data posted successfully');
+      })*/
+
+});
---
+++ js/word.js
@@ -0,0 +1,14 @@
+/*var app = angular.module('word', [editor]);
+app.controller('x', function($scope) {
+   $scope.submit= function(){
+    /*  var data = $.param({
+        var tmp=$scope.latex_code;
+      }) 
+      //});
+      console.log(data)
+      //$http.post("/api/book/", data).success(function(data, status) {
+      //  console.log('Data posted successfully');
+    //  })
+  }
+});
+*/
