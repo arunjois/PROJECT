a4845f7062281e6515796796acd37388fa8b8972
Arun Jois <arun.joiss@gmail.com>
2018-07-14 13:25:09 +0530

    Backup Commit

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var fs = require('fs');
-var app = require('express');
-var connect = require('connect');
-var publicPath = path.resolve(__dirname, '');
-var express=connect();
-express.use(app.static(publicPath));
+var app = express();
+var publicPath = "/opt/lampp/htdocs/PROJECT/";
+app.use(express.static(publicPath));
+app.use(publicPath + './img/*', express.static(__dirname + './img/*'));
+app.get("/RVCE_logo.jpg",function(req,res){
+  fs.readFile("./img/RVCE_logo.jpg",function(err,data){
+    res.writeHead(200, {'Content-Type': 'image/html','Content-Length':data.length});
+    res.write(data);
+    res.end();
+  })
+});
 http.createServer(function(req, res){
-    fs.readFile('./word.html',function (err, data){
+    fs.readFile('word.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
