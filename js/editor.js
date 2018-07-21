ed743ad9ca9ab97c55783c724fd38d82baf2b280
Arun Jois <arun.joiss@gmail.com>
2018-07-21 13:52:02 +0530

    [REWRITE] Deleted Everything

--- js/editor.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('editor', ['editor']);
-app.controller("code", function($scope) {
-   $scope.latex="RVCE";
-
-    /*  $http.post("/api/book/", data).success(function(data, status) {
-        console.log('Data posted successfully');
-      })*/
-
-});
