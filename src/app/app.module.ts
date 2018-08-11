902a05cfd0b8c16d6049f4f7a557b330034242d9
Arun Jois <arun.joiss@gmail.com>
2018-08-11 17:05:04 +0530

    [PATCH] Grid Done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule} from '@angular/material/menu';
 
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
+import { ContollerComponent } from './grid/contoller/contoller.component';
+import { TreeComponent } from './grid/tree/tree.component';
+import { ViewComponent } from './grid/view/view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridComponent
+    GridComponent,
+    ContollerComponent,
+    TreeComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
-    MatGridListModule
+    MatGridListModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
