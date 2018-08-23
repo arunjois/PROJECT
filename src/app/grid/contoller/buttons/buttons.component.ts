c86289dba1a534bb6f03f1241c275cc0a959925c
Arun Jois <arun.joiss@gmail.com>
2018-08-23 11:26:02 +0530

    [PATCH] Button functionality Added

--- src/app/grid/contoller/buttons/buttons.component.ts
+++ src/app/grid/contoller/buttons/buttons.component.ts
@@ -9,6 +9,40 @@ import { Component, OnInit } from '@angular/core';
 
 export class ButtonsComponent implements OnInit {
 
+/* 
+ * Method to Modifiy the Document 
+*/
+
+  addSection() {
+    console.log("Add Section");
+  }
+  
+
+  addSubSection() {
+    console.log("Add Sub Section")
+  }
+
+  addList() {
+    console.log("Add List");
+  }
+  
+
+  addTable() {
+    console.log("Add Table");
+  }
+
+  addImage() {
+    console.log("Add Image");
+  }
+
+  addFraction() {
+    console.log("Add Fraction");
+  }
+  
+  addBullets(){
+    console.log("Add Bullets");
+  }
+
   constructor() { }
 
   ngOnInit() {
